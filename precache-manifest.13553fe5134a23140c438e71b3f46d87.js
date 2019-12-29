2525fef0323f94522b4633bebec97335d239dd2f
Eugene Revutskiy <revytskiy.evgen@gmail.com>
2019-12-29 11:51:51 +0200

    Updates

---
+++ precache-manifest.13553fe5134a23140c438e71b3f46d87.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fb18f59d982710a48386439723d009d",
+    "url": "/lerestaurante/index.html"
+  },
+  {
+    "revision": "8c82909bf71cd1d76c69",
+    "url": "/lerestaurante/static/css/2.2e09a85b.chunk.css"
+  },
+  {
+    "revision": "c38b1a6ff9f23d44d339",
+    "url": "/lerestaurante/static/css/main.8d344182.chunk.css"
+  },
+  {
+    "revision": "8c82909bf71cd1d76c69",
+    "url": "/lerestaurante/static/js/2.cdebf4ce.chunk.js"
+  },
+  {
+    "revision": "1aff40f6cd2589c1f06af7e73a2046f9",
+    "url": "/lerestaurante/static/js/2.cdebf4ce.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c38b1a6ff9f23d44d339",
+    "url": "/lerestaurante/static/js/main.991e2fe1.chunk.js"
+  },
+  {
+    "revision": "54ece616bd304eb451a9",
+    "url": "/lerestaurante/static/js/runtime-main.c0fb1396.js"
+  },
+  {
+    "revision": "25407b16574662d326a2935bc2dda46b",
+    "url": "/lerestaurante/static/media/1.25407b16.png"
+  },
+  {
+    "revision": "373a3e8bde21c04a234c2c79b9e56057",
+    "url": "/lerestaurante/static/media/2.373a3e8b.png"
+  },
+  {
+    "revision": "c18388815fe3f8eeabec3b95500e6f70",
+    "url": "/lerestaurante/static/media/3.c1838881.png"
+  },
+  {
+    "revision": "7dbe772383c8ad96bc59c5e3fa5520e4",
+    "url": "/lerestaurante/static/media/4.7dbe7723.png"
+  },
+  {
+    "revision": "ec2099f3d455efbec2ba63efc5dd6d7d",
+    "url": "/lerestaurante/static/media/5.ec2099f3.png"
+  },
+  {
+    "revision": "9fc05eac85731340e269333bdda9882c",
+    "url": "/lerestaurante/static/media/6.9fc05eac.png"
+  },
+  {
+    "revision": "9b498dd10f6296375d97c1b18438ea3d",
+    "url": "/lerestaurante/static/media/address.9b498dd1.jpg"
+  },
+  {
+    "revision": "76055221886c7b857b4cf612c44aa2d3",
+    "url": "/lerestaurante/static/media/book-bg.76055221.jpg"
+  },
+  {
+    "revision": "c9304e612f787e41db12e51520863dc1",
+    "url": "/lerestaurante/static/media/chiken.c9304e61.jpg"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/lerestaurante/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/lerestaurante/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/lerestaurante/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/lerestaurante/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/lerestaurante/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/lerestaurante/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/lerestaurante/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/lerestaurante/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/lerestaurante/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/lerestaurante/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/lerestaurante/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/lerestaurante/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/lerestaurante/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/lerestaurante/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/lerestaurante/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "7f6985ab952b5ee2cc2e7ac47e318ff6",
+    "url": "/lerestaurante/static/media/links.7f6985ab.jpg"
+  },
+  {
+    "revision": "181ee90603f79039c886484d175fa242",
+    "url": "/lerestaurante/static/media/meat.181ee906.jpg"
+  },
+  {
+    "revision": "87a80a27e79687b4520a3ccead6b18f2",
+    "url": "/lerestaurante/static/media/meat.87a80a27.jpg"
+  },
+  {
+    "revision": "2aba2169802c1f05e20e966966cc6007",
+    "url": "/lerestaurante/static/media/pasta.2aba2169.jpg"
+  },
+  {
+    "revision": "ad7001fc7a263f65e0ce20a8a292a894",
+    "url": "/lerestaurante/static/media/restaurant.ad7001fc.jpg"
+  },
+  {
+    "revision": "832936f14091c02cefcc6a7840234ece",
+    "url": "/lerestaurante/static/media/salad.832936f1.jpg"
+  },
+  {
+    "revision": "8b908ae1b0e9c35e86d812ebf61fab9f",
+    "url": "/lerestaurante/static/media/secrets.8b908ae1.jpg"
+  },
+  {
+    "revision": "5be03fb4ac68588ba09f7f9520a27da9",
+    "url": "/lerestaurante/static/media/soup.5be03fb4.jpg"
+  },
+  {
+    "revision": "88dc0a244412c97ab9649b079a85d329",
+    "url": "/lerestaurante/static/media/trout.88dc0a24.jpg"
+  },
+  {
+    "revision": "e990a000c6d0a71cce4b80374cd5e0df",
+    "url": "/lerestaurante/static/media/vegeterian.e990a000.jpg"
+  }
+]);
